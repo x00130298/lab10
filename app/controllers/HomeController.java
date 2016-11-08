3068d43b1538ac49aa1e6844fde50c7e7c63d4b0
Enda Lee <enda.lee@ittdublin.ie>
2016-11-08 13:57:37 +0000

    Start

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import play.api.Environment;
+import play.mvc.*;
+import play.data.*;
+import play.db.ebean.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+
+import views.html.*;
+
+// Import models
+import models.*;
+
+
+public class HomeController extends Controller {
+
+    public Result index(String name) {
+        return ok(index.render("Welcome to the Home page", name));
+    }
+
+    public Result about() {
+        return ok(about.render());
+    }
+
+    public Result products() {
+
+        // Get list of all categories in ascending order
+        List<Product> productsList = Product.findAll();
+        return ok(products.render(productsList));
+    }
+
+}
+
+
